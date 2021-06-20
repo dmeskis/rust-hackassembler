77846d0c2af46f3203e299abb05ed0c1b8ab28b8
Dylan <dmeskis@gmail.com>
2021-06-20 15:53:48 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -246,14 +246,27 @@ pub fn run(args: &[String]) -> Result<(), Box<dyn Error>> {
 
     let parser = Parser { commands };
 
+    // let result = parser.commands.flat_map(|command|
+    //     [command.dest(), command.comp(), command.jump()]
+    //     ).join("");
+    //
+    let mut result = vec![];
     for command in parser.commands {
+        let dest = Command::bdest(command.dest());
+        let comp = Command::bcomp(command.comp());
+        let jump = Command::bjump(command.jump());
+        result.push(dest);
+        result.push(comp);
+        result.push(jump);
         // println!(" {:?}", command.dest());
         // println!(" {:?}", command.comp());
         // println!(" {:?}", command.jump());
-        println!(" {:?}", Command::bdest(command.dest()));
-        println!(" {:?}", Command::bcomp(command.comp()));
-        println!(" {:?}", Command::bjump(command.jump()));
+        // println!(" {:?}", Command::bdest(command.dest()));
+        // println!(" {:?}", Command::bcomp(command.comp()));
+        // println!(" {:?}", Command::bjump(command.jump()));
     }
+
+    println!("{:?}", result.join(""));
 
     Ok(())
 }
